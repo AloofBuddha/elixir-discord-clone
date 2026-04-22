8e13fef321b2a0188f88c2a176421df74b624f1a
Benjamin Cohen <benjamin@codeplatoon.org>
2026-04-22 15:57:17 -0600

    forgot from last push

---
+++ lib/discord/servers/server.ex
@@ -0,0 +1,19 @@
+defmodule Discord.Servers.Server do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "servers" do
+    field :name, :string
+    belongs_to :owner, Discord.Accounts.User
+    has_many :channels, Discord.Servers.Channel
+    has_many :memberships, Discord.Servers.Membership
+    timestamps()
+  end
+
+  def changeset(server, attrs) do
+    server
+    |> cast(attrs, [:name, :owner_id])
+    |> validate_required([:name, :owner_id])
+    |> validate_length(:name, min: 1, max: 100)
+  end
+end
